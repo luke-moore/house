b886aaaa6e7bcf0b60990d3602df0c3e1da5512d
luke.moore <Luke Moore>
2015-11-30 23:53:51 -0500

    Discovering wemo devices when starting.
    Otherwise, the switch cannot be found.

--- django/wemo/models.py
+++ django/wemo/models.py
@@ -1,4 +1,5 @@
 import ouimeaux.environment
+import psutil
 
 _cached_environment = None
 def _environment():
@@ -6,8 +7,16 @@ def _environment():
     if _cached_environment is None:
         _cached_environment = ouimeaux.environment.Environment()
         _cached_environment.start()
+        _cached_environment.discover(1)
 
     return _cached_environment
+
+def _is_ouimeaux_server_running():
+    for connection in psutil.net_connections():
+        if connection.laddr[1] == 8989 and connection.status == "LISTEN":
+            return True
+
+    return False
 
 def _switch(switch_name):
     return _environment().get_switch(switch_name)
